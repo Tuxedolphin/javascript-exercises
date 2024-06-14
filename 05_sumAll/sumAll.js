937bebce93ffd285f49d4cb5285b7a14415c3653
Tuxedolphin <zhuzhen.zhuo@gmail.com>
2024-06-14 10:40:26 +0800

    Finished Exercises for Lesson On Arrays And Loops

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,27 @@
-const sumAll = function() {
+const sumAll = function(numb_1, numb_2) {
+    if (typeof(numb_1) !== "number" || typeof(numb_2) !== "number") {
+        return "ERROR";
+    }
+
+    if (numb_1 < 0 || numb_2 < 0) {
+        return "ERROR";
+    }
+
+    if (numb_1 < numb_2) {
+        smaller_number = numb_1;
+        larger_number = numb_2;
+    } else {
+        smaller_number = numb_2;
+        larger_number = numb_1;
+    }
+
+    result = 0;
+
+    for (let i = smaller_number; i <= larger_number; i++) {
+        result += i
+    }
+
+    return result
 
 };
 
--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,13 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (!(year % 4)) {
+        if (!(year % 100)) {
+            return !(year % 400) ? true : false
+        }
 
+        return true
+    }
+
+    return false
 };
 
 // Do not edit below this line
